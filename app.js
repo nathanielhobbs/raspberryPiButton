d01785772feb05b88be93185a485de719b1e7e1c
Nathan Hobbs <hobbs.nathaniel@gmail.com>
2016-04-30 00:02:07 +0800

    added ejs for rendering pages

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+"use strict"
+
 var Gpio = require('onoff').Gpio;
 var express = require('express');
 var app = express();
 var settings = require('./settings');
 var port = settings.port;
 
+// asign led as output of GPIO pin 18
 var led = new Gpio(18, 'out');
 
 // establish streams for RaspPi components
@@ -15,10 +18,13 @@ var led = new Gpio(18, 'out');
 // var logger = require('morgan');
 // app.use(logger('common'));
 
-// set up body parser middleware POST data
+// set up body parser middleware to handle POST data
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({extended:false}));
+
+// set view engine for rendering 
+app.set('view engine', 'ejs');
 
 // pipe button presses to led
 // var write_stream = button.pipe(led);
@@ -28,11 +34,19 @@ app.use(bodyParser.urlencoded({extended:false}));
 // API routes
 app.post('/plants', function(req,res) {
   console.log('new plant:');
+  console.log(JSON.stringify(req.body))
+  if(parseInt(req.body.name) === 1){
+    led.writeSync(1);
+  }
+  else if(parseInt(req.body.name) === 0){
+    led.writeSync(0);
+  }
 });
 
 app.get('/', function(req,res){
    // res.send("Welcome to Nathaniel's Plant Monitoring System");
-   res.sendFile(__dirname + '/public/index.html');
+   // res.sendFile(__dirname + '/public/index.html');
+   res.status(200).render('index')
    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
    console.log('Status : ' + led.readSync() + ' - ip : ' + ip);
    led.writeSync(0);
